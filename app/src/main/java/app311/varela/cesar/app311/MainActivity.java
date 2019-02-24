a080f15c8a41cb52fd16ed61fe652b49413911c8
cesarvarela07 <47933793+cesarvarela07@users.noreply.github.com>
2019-02-23 22:12:40 -0600

    Initial commit

---
+++ app/src/main/java/app311/varela/cesar/app311/MainActivity.java
@@ -0,0 +1,23 @@
+package app311.varela.cesar.app311;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        findViewById(R.id.registrarseBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Registro.class));
+            }
+        });
+    }
+}
