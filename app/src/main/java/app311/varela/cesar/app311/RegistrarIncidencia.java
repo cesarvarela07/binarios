897379b984ad98c063a0ab3441b5235dac1f15a3
cesarvarela07 <47933793+cesarvarela07@users.noreply.github.com>
2019-04-12 19:09:06 -0600

    Last

--- app/src/main/java/app311/varela/cesar/app311/RegistrarIncidencia.java
+++ app/src/main/java/app311/varela/cesar/app311/RegistrarIncidencia.java
@@ -1,19 +1,67 @@
 package app311.varela.cesar.app311;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.os.Parcelable;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class RegistrarIncidencia extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     ArrayList<String> provincia =new ArrayList<>();
     ArrayList<String> canton =new ArrayList<>();
     ArrayList<String> distrito =new ArrayList<>();
+    ImageView addPhoto = (ImageView) findViewById(R.id.imgPhotoUser);
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+            if (data.getExtras()!= null) {
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                addPhoto.setImageBitmap(imageBitmap);
+            } else {
+                Uri selectedimg = data.getData();
+                try {
+                    Bitmap mBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedimg);
+                    int width = mBitmap.getWidth();
+                    int height = mBitmap.getHeight();
+                    float scaleWidth = ((float) 300) / width;
+                    float scaleHeight = ((float) 400) / height;
+                    // create a matrix for the manipulation
+                    Matrix matrix = new Matrix();
+                    // resize the bit map
+                    matrix.postScale(scaleWidth, scaleHeight);
+                    // recreate the new Bitmap
+                    Bitmap bit = Bitmap.createBitmap(mBitmap, 0, 0, width, height, matrix, false);
+                    addPhoto.setImageBitmap(bit);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -113,4 +161,8 @@ public class RegistrarIncidencia extends AppCompatActivity {
         distrito.add("Escazú");
 
     }
+
+
+
+
 }
