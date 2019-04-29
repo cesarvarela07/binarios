e7f8ed4506505ef7fa76a6e2794437e789db9fab
cesarvarela07 <47933793+cesarvarela07@users.noreply.github.com>
2019-04-28 23:57:49 -0600

    endgame/28-04

--- app/src/main/java/app311/varela/cesar/app311/RegistrarIncidencia.java
+++ app/src/main/java/app311/varela/cesar/app311/RegistrarIncidencia.java
@@ -14,38 +14,22 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ToggleButton;
 import android.location.Location;
 import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
-import android.location.Location;
 import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.provider.MediaStore;
 
 
 
 import java.util.ArrayList;
 
 public class RegistrarIncidencia extends AppCompatActivity {
-    ArrayList<String> provincia =new ArrayList<>();
-    ArrayList<String> canton =new ArrayList<>();
-    ArrayList<String> distrito =new ArrayList<>();
     ArrayList<String> empresa =new ArrayList<>();
     ArrayList<String> categoria =new ArrayList<>();
     ImageView img;
@@ -53,12 +37,9 @@ public class RegistrarIncidencia extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
     LocationManager locationManager;
-    double longitudeBest, latitudeBest;
+
     double longitudeGPS, latitudeGPS;
-    double longitudeNetwork, latitudeNetwork;
-    TextView longitudeValueBest, latitudeValueBest;
     TextView longitudeValueGPS, latitudeValueGPS;
-    TextView longitudeValueNetwork, latitudeValueNetwork;
 
 
     @Override
@@ -82,69 +63,12 @@ public class RegistrarIncidencia extends AppCompatActivity {
         });
 
 
-
-
         //SPINNER
-        Spinner prov= (Spinner) findViewById(R.id.Provincia);
-        Spinner cant= (Spinner) findViewById(R.id.Canton);
-        Spinner dist= (Spinner) findViewById(R.id.Distrito);
         Spinner empr= (Spinner) findViewById(R.id.Empresa);
         Spinner cate= (Spinner) findViewById(R.id.Categoria);
 
         //FILL DATA
         fillData();
-
-        //ADAPTR
-        ArrayAdapter<String> pro=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,provincia);
-        prov.setAdapter(pro);
-
-        prov.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(RegistrarIncidencia.this, provincia.get(i), Toast.LENGTH_SHORT).show();
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-
-        //ADAPTR
-        ArrayAdapter<String> can=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,canton);
-        cant.setAdapter(can);
-
-        cant.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(RegistrarIncidencia.this, canton.get(i), Toast.LENGTH_SHORT).show();
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-
-
-        //ADAPTR
-        ArrayAdapter<String> dis=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,distrito);
-        dist.setAdapter(dis);
-
-        dist.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(RegistrarIncidencia.this, distrito.get(i), Toast.LENGTH_SHORT).show();
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
 
         //ADAPTR
         ArrayAdapter<String> emp=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,empresa);
@@ -279,33 +203,6 @@ public class RegistrarIncidencia extends AppCompatActivity {
     //FILL DATA
     private void fillData()
     {
-        provincia.clear();
-
-        //FILL
-        provincia.add("San José");
-        provincia.add("Heredia");
-        provincia.add("Cartago");
-        provincia.add("Alajuela");
-        provincia.add("Limón");
-        provincia.add("Punturenas");
-        provincia.add("Guanacaste");
-
-
-        canton.clear();
-
-        //FILL
-        canton.add("San José");
-        canton.add("Desamparados");
-        canton.add("Escazú");
-
-
-
-        distrito.clear();
-
-        //FILL
-        distrito.add("San José");
-        distrito.add("Desamparados");
-        distrito.add("Escazú");
 
         empresa.clear();
 
@@ -329,83 +226,3 @@ public class RegistrarIncidencia extends AppCompatActivity {
 
 
 }
-
-//        // Here, we are making a folder named picFolder to store
-//        // pics taken by the camera using this application.
-//        final String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/picFolder/";
-//        File newdir = new File(dir);
-//        newdir.mkdirs();
-//
-//        Button capture = (Button) findViewById(R.id.btnCapture);
-//        capture.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//
-//                // Here, the counter will be incremented each time, and the
-//                // picture taken by camera will be stored as 1.jpg,2.jpg
-//                // and likewise.
-//                count++;
-//                String file = dir+count+".jpg";
-//                File newfile = new File(file);
-//                try {
-//                    newfile.createNewFile();
-//                }
-//                catch (IOException e)
-//                {
-//                }
-//
-//                Uri outputFileUri = Uri.fromFile(newfile);
-//
-//                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
-//
-//                startActivityForResult(cameraIntent, TAKE_PHOTO_CODE);
-//            }
-//        });
-//    }
-//
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        if (requestCode == TAKE_PHOTO_CODE && resultCode == RESULT_OK) {
-//            Log.d("CameraDemo", "Pic saved");
-//        }
-//    }
-
-
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        try {
-//
-//            super.onActivityResult(requestCode, resultCode, data);
-//            if (requestCode != 0 && data != null) {
-//
-//                if (data.getExtras()!= null) {
-//                    Bundle extras = data.getExtras();
-//                    Bitmap imageBitmap = (Bitmap) extras.get("data");
-//                    addPhoto.setImageBitmap(imageBitmap);
-//                } else {
-//                    Uri selectedimg = data.getData();
-//                    try {
-//                        Bitmap mBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedimg);
-//                        int width = mBitmap.getWidth();
-//                        int height = mBitmap.getHeight();
-//                        float scaleWidth = ((float) 300) / width;
-//                        float scaleHeight = ((float) 400) / height;
-//                        // create a matrix for the manipulation
-//                        Matrix matrix = new Matrix();
-//                        // resize the bit map
-//                        matrix.postScale(scaleWidth, scaleHeight);
-//                        // recreate the new Bitmap
-//                        Bitmap bit = Bitmap.createBitmap(mBitmap, 0, 0, width, height, matrix, false);
-//                        addPhoto.setImageBitmap(bit);
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            }
-//
-//        }catch(Exception e){
-//
-//        }
-//    }
